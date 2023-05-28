3915e7f6978dd48572c07ec6e5d070c654f86e26
Habibaelkabbany <helkabbany2@gmail.com>
2023-05-29 02:16:55 +0300

    not done :(

--- src/Views/viewTransaction.js
+++ src/Views/viewTransaction.js
@@ -3,7 +3,10 @@ const existing = ["Hussein H", "Zeyad Z", "Layla LL"];
 const emails = ["hhh@gmail", "zz@yahoo.com", "lolo@gmail.com"];
 const names = ["hussein hany", "zeyad zeyad", "layla layaly"];
 const accounts = [129029383928, 3789232987, 93847823787];
-
+const trans1 =  { "19202",  "1/1/2023",  "credit", 100, DebitAmount: 0, Balance: 10000, Description: "Transfer" };
+const trans2 = { "19212",  "10/11/2022",  "debit",  0, DebitAmount: 250, Balance: 10250, Description: "Bill Payment" };
+const trans3 = { "19222",  "1/5/2023",  "credit",  9990, DebitAmount: 0, Balance: 10, Description: "Transfer" };
+const myTransactions = { trans1, trans2, trans3 };
 const select = document.getElementById("mySelectSource");
 
 for (let i = 0; i < myArray.length; i++) {
@@ -13,18 +16,41 @@ for (let i = 0; i < myArray.length; i++) {
     select.add(option);
 
 }
-function updateTable(){
-    const selectedValue= select.value;
+function updateTable() {
+    const selectedValue = select.value;
     var table = document.getElementById("myTable");
     var tbody = table.getElementsByTagName("tbody")[0];
-    tbody.innerHTML = "";
-    for (let i=0;i<myArray.length;i++){
-        if (selectedValue === ""){
-              
- 
-        }
-        if(selectedValue === myArray[i]){
+    table.innerHTML = "";
+    for (let i = 0; i < myArray.length; i++) {
+        if (selectedValue === "") {
 
+            table.hide();
         }
+        if (selectedValue === myArray[i]) {
+            for (var i = 0; i < myTransactions.length; i++) {
+                var row = table.insertRow();
+            }for(var j = 0; j < 7; j++) {
+                var cell = row.insertCell();
+                if (i)
+                cell.innerHTML = "Row " + (i+1) + ", Column " + (j+1);
+              }
+        }
+
+    }
+
+
+    // Clear the current table
+    table.innerHTML = "";
+
+    // Create a new table based on the selected option
+
+    // Create a 2x2 table
+
+    for (var j = 0; j < 2; j++) {
+        var cell = row.insertCell();
+        cell.innerHTML = "Row " + (i + 1) + ", Column " + (j + 1);
     }
 }
+break;
+        
+      case "option2":
