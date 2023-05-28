9da94c5f942944b5faea04be1c908472813f53e1
RamadanO007 <omaryasser5922@gmail.com>
2023-05-28 18:24:09 +0300

    testoo

--- src/Login_Reg/Regpage.js
+++ src/Login_Reg/Regpage.js
@@ -1,43 +1,31 @@
 
-    function register() {
-      // Retrieve input values
-      var name = document.getElementById("name").value;
-      var email = document.getElementById("email").value;
-      var phone = document.getElementById("phone").value;
-      var account = document.getElementById("account").value;
-      var nationalid = document.getElementById("nationalid").value;
+function register() {
+  // Retrieve input values
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var phone = document.getElementById("phone").value;
+  var account = document.getElementById("account").value;
+  var nationalid = document.getElementById("nationalid").value;
 
-      // Dummy data for registration
-      var dummyData = [
-        {
-          name: "omar",
-          email: "omar@gmail.com",
-          phone: "01066800034",
-          account: "0112345678",
-          nationalid: "1234567890123"
-        }
-        // Add more dummy data if needed
-      ];
-
-      // Check if input matches the dummy data
-        if (
-          name === dummyData.name &&
-          email === dummyData.email &&
-          phone === dummyData.phone &&
-          account === dummyData.account &&
-          nationalid === dummyData.nationalid
-        ) {
-          alert("Registration Complete!");
-          // window.location.href = "Login.html";
-          // location.replace("Login.html");
-        }
-      
-else{
-  alert("Invalid Registration data. Please try again.");
-
+  // Check if phone number, bank account, and national ID have the correct number of digits
+  if (phone.length === 11 && account.length === 10 && nationalid.length === 14) {
+    alert("Registration Complete!");
+    window.location.href = "Login.html";
+  } else {
+    alert("Invalid Registration data. Please try again.");
+  }
 }
-      // If input data doesn't match, display an error message
-    }
+
+function redirectToLogin() {
+  var redirectLocation = "Login.html";
+  redirectTo(redirectLocation);
+}
+
+function redirectTo(url) {
+  // Redirect to a different URL
+  window.location.replace(url);
+}
+
 
     function redirectToLogin() {
       var redirectLocation = "Login.html";
